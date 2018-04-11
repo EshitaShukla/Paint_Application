4141cf1f05f98477c35229d88f6085897cceed77
Eshita Shukla <32108487+EshitaShukla@users.noreply.github.com>
2018-04-11 20:14:36 +0530

    Add files via upload

--- paint_application.py
+++ paint_application.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env/python3
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+from PIL import Image
+import pyscreenshot as ImageGrab
+
+#import drawPattern as draw
+
 
 from tkinter.colorchooser import *
 trace = 0 
 
 class CanvasEventsDemo:
     
-    def __init__(self, x,cb,parent=None):
-        canvas = Canvas(x,height=1000,width=2000,bg=cb) 
+    def __init__(self, r,cb,parent=None):
+        canvas = Canvas(r,height=1000,width=2000,bg=cb) 
         canvas.grid(row=2,column=0,columnspan=2000)
         canvas.bind('<ButtonPress-1>', self.onStart) 
         canvas.bind('<B1-Motion>',     self.onGrow)  
@@ -20,17 +26,39 @@ class CanvasEventsDemo:
         x=IntVar()
         self.w=Scale(r,variable=x,orient="horizontal",background="dimgrey")
         self.w.grid(row=1,column=8)
-        
+                
         def ova():
+            self.c=0
             self.kinds=canvas.create_oval
+            canvas.bind('<ButtonPress-1>', self.onStart) 
+            canvas.bind('<B1-Motion>',     self.onGrow)  
+            canvas.bind('<Double-1>',      self.onClear) 
+            canvas.bind('<ButtonPress-3>', self.onMove)
+ 
         def rect():
             self.kinds=canvas.create_rectangle
-            c=0
+            self.c=0
+            canvas.bind('<ButtonPress-1>', self.onStart) 
+            canvas.bind('<B1-Motion>',     self.onGrow)  
+            canvas.bind('<Double-1>',      self.onClear) 
+            canvas.bind('<ButtonPress-3>', self.onMove)
+ 
         def line():
+            self.c=1
             self.kinds=canvas.create_line
-        def draw():
+            canvas.bind('<ButtonPress-1>', self.onStart) 
+            canvas.bind('<B1-Motion>',     self.onGrow)  
+            canvas.bind('<Double-1>',      self.onClear) 
+            canvas.bind('<ButtonPress-3>', self.onMove)
+ 
+        def drawit():
             self.kinds=canvas.create_rectangle
-            c=1
+            self.c=1
+            canvas.bind('<ButtonPress-1>', self.dx) 
+            canvas.bind('<B1-Motion>',     self.dx)  
+            canvas.bind('<Double-1>',      self.dx) 
+            canvas.bind('<ButtonPress-3>', self.dx)
+            
         def cbg():
             cb=askcolor()
             canvas.configure(bg=cb[1])
@@ -38,12 +66,26 @@ class CanvasEventsDemo:
             cx=askcolor()
             self.cl=cx[1]
             return self.cl
+            
+        def getter():
+            x2=r.winfo_rootx()+canvas.winfo_x()
+            y2=r.winfo_rooty()+canvas.winfo_y()
+            x1=x2+canvas.winfo_width()
+            y1=y2+canvas.winfo_height()
+            I=ImageGrab.grab().crop((x2,y2,x1,y1))
+            
+            filename=filedialog.askdirectory()
+            print(filename)
+            I.save(str(filename)+"/test.jpg")
+            
         # Shapes
-        b1=Menu(r,title="    Oval    ",font="ubuntu 15",background="dimgrey",foreground="deepskyblue")
-        b1.add_command(label="     oval     ",command=lambda: ova()) 
-        b1.add_command(label="     Rectangle     ", command=lambda: rect()) 
-        b1.add_command(label="     Line    ", command=lambda: line()) 
-        
+        b1=Menu(r,title="    Oval    ",font="ubuntu 10",background="dimgrey",foreground="snow")
+        b1.add_command(label="       Oval         ",command=lambda: ova()) 
+        b1.add_command(label="     Rectangle    ", command=lambda: rect()) 
+        b1.add_command(label="       Line         ", command=lambda: line()) 
+        b1.add_command(label="       Draw         ", command=lambda: drawit()) 
+        b1.add_command(label="       Save          ",command=lambda:getter())
+        b1.add_command(label="fill",command=lambda:canvas.bind('<B1-Motion>',     self.fill) ) 
         #separator
             #f=ttk.Separator(orient="vertical")
             #f.place(x=85, relheight=1)
@@ -52,7 +94,7 @@ class CanvasEventsDemo:
         b1.add_command(label="""  choose color  """,command=lambda: col())
 
         #bg color
-        b1.add_command(label="""bg color""",command=lambda: cbg())
+        b1.add_command(label="""background      """,command=lambda: cbg())
         
         r.config(menu=b1)
         self.canvas = canvas
@@ -73,10 +115,14 @@ class CanvasEventsDemo:
             canvas.delete(self.drawn)
         if self.kinds==canvas.create_line:
             objectId = self.shape(self.start.x, self.start.y, event.x, event.y,fill=self.cl,width=(self.w.get()+1)/10)
-            print(self.w.get())
+            
         else:
-            objectId = self.shape(self.start.x, self.start.y, event.x, event.y,outline=self.cl,width=(self.w.get()+1)/10)
-            print(self.w.get())
+            if self.c==1:
+                objectId = self.shape(self.start.x, self.start.y,self.start.x+2,self.start.y+2)
+            else:
+                objectId = self.shape(self.start.x, self.start.y, event.x, event.y,outline=self.cl,width=(self.w.get()+1)/10)
+            
+            
 #        if trace:
 #            print (objectId)
         
@@ -93,7 +139,27 @@ class CanvasEventsDemo:
             diffX, diffY = (event.x - self.start.x), (event.y - self.start.y)
             canvas.move(self.drawn, diffX, diffY)
             self.start = event
-            
+    
+    def dx(self,event):
+        canvas=event.widget
+        def paint(event):
+            width=(self.w.get()+1)/20
+            x1, y1 = ( event.x - width ), ( event.y - width  )
+            x2, y2 = ( event.x +width  ), ( event.y + width  )
+            canvas.create_rectangle( x1, y1, x2, y2, fill = self.cl,outline=self.cl )
+        canvas.bind( "<B1-Motion>", paint )
+        
+"""    def fill(self,event):
+        canvas=event.widget
+        self.canvas.bind("<ButtonPress-1>", self.on_fill_color)
+    
+    def on_fill_color(self, event):
+        
+        self.filled = draw.fillColor(self.paper, (event.x, event.y), self.bgColor, self.chooseColor, self.paperWidth, self.paperHeight)
+        self.canvas.create_image(self.paperWidth / 2, self.paperHeight / 2, image=self.filled)
+        print("fill")
+ """       
+        
 r=tk.Tk()
 r.config(background="black")
 cb="snow"
@@ -101,5 +167,3 @@ cb="snow"
 c=CanvasEventsDemo(r,cb)
 r.mainloop()
 
-
-           
